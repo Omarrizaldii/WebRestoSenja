d18cd660c06e60d1232fec87e951029add60de0e
omarrizaldii <omarrzld1982@gmail.com>
2023-06-01 21:40:19 +0700

    add swager token

--- CoffeSenja.API/Program.cs
+++ CoffeSenja.API/Program.cs
@@ -1,3 +1,12 @@
+using CoffeSenja.API.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
+Console.Title = "CoffeSenja API";
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +14,61 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddDbContext<CoffeSenjaContext>(s => s.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
+builder.Services.AddSwaggerGen(x =>
+{
+    x.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "CoffeSenja - API v1",
+        Version = "v1",
+        Description = "Welcome to CoffeSenja API",
+        Contact = new OpenApiContact
+        {
+            Name = "CoffeSenja"
+        }
+    });
+
+    x.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Description = "JWT Authorization header using the Bearer scheme.\r\n\r\n " +
+        "Enter your token in the text input below.\r\n\r\n Example: 'Bearer eyabcdef12345'",
+        In = ParameterLocation.Header,
+        BearerFormat = "JWT",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    x.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            }, new string []{}
+        }
+    });
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateLifetime = true,
+        ValidateAudience = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"])),
+        ClockSkew = TimeSpan.Zero
+    };
+});
 
 var app = builder.Build();
 
@@ -13,11 +76,16 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(x =>
+    {
+        x.SwaggerEndpoint("/swagger/v1/swagger.json", "POSCafe API v1");
+        x.DefaultModelExpandDepth(-1);
+    });
 }
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
--- CoffeSenja.API/Utilities/BaseService.cs
+++ CoffeSenja.API/Utilities/BaseService.cs
@@ -1,9 +1,46 @@
-﻿namespace CoffeSenja.API.Utilities
+﻿using CoffeSenja.API.Models;
+
+namespace CoffeSenja.API.Utilities
 {
     public class BaseService
     {
-        public BaseService()
+        public CoffeSenjaContext context;
+        private readonly IHttpContextAccessor httpContext;
+        private readonly IWebHostEnvironment webHost;
+
+        public BaseService(CoffeSenjaContext context, IConfiguration configuration, IHttpContextAccessor httpContext, IWebHostEnvironment webHost, string email)
         {
+            this.context = context;
+            Customers = context.Customers.Where(s => s.Email == email).FirstOrDefault();
+            if (Customers == null)
+                Customers = new Customer();
+            this.httpContext = httpContext;
+            this.webHost = webHost;
         }
+
+        public ResponseCode response(int code, string? message, object? data)
+        {
+            try
+            {
+                return new ResponseCode
+                {
+                    StatusCode = code,
+                    Message = message,
+                    Data = data
+                };
+            }
+            catch (Exception x)
+            {
+                return new ResponseCode
+                {
+                    StatusCode = 500,
+                    Message = x.Message,
+                    Data = x.InnerException?.Message
+                };
+            }
+        }
+
+        public Customer Customers
+        { get; set; }
     }
 }
